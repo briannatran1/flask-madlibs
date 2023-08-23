02e4324591721eb59e9f801221dc70e535d7bb0e
Noah Appelbaum <noahappelbaum@gmail.com>
2023-08-23 18:02:56 -0500

    wrote view function and template for questions

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get("/")
+def show_form():
+    """Shows form based story instance"""
+
+    return render_template("questions.html", prompts=silly_story.prompts)
